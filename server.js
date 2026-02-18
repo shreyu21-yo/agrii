216e73291882cd885811ab207254f481b3924cd6
shreyu21-yo <shreyashreyu21@gmail.com>
2026-02-18 22:14:39 +0530

    Refactor server.js to use environment variables

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+require('dotenv').config();
+
 const mongoose = require("mongoose");
-require("dotenv").config();
 
-const authRoutes = require("./routes/auth");
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-mongoose.connect("mongodb://127.0.0.1:27017/agriconnect")
-  .then(() => console.log("MongoDB connected"));
-
-app.use("/api/auth", require("./routes/auth"));
-
-
-app.listen(5000, () => console.log("Server running on 5000"));
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
