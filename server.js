a2d0a8d81753afc78166673e0d169bcb1ec11027
shreyu21-yo <shreyashreyu21@gmail.com>
2026-02-18 22:26:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,16 +3,21 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const authRoutes = require("./routes/auth");
-
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/agriconnect")
-  .then(() => console.log("MongoDB connected"));
+// ✅ CONNECT TO MONGODB ATLAS FROM RENDER ENV
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
+// ✅ ROUTES
 app.use("/api/auth", require("./routes/auth"));
 
+// ✅ RENDER PORT FIX
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log("Server running on 5000"));
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
